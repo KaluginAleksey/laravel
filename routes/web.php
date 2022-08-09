59f80f2a41b6ec619c22804b0f1f8cc2b2892a67
KaluginAleksey <mr.kamc@yandex.ru>
2022-08-09 14:03:13 +0300

    Edit rout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TestController as TestControllerAlias;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', TestControllerAlias::class);
+Route::get('/test', TestController::class);
 
 
 
